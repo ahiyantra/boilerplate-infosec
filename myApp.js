51184c9ae6a76e727ed17c3b5724ee4b0519923b
ahiyantra <45235557+ahiyantra@users.noreply.github.com>
2023-12-30 07:41:19 +0530

    updated for deployment

--- myApp.js
+++ myApp.js
@@ -2,7 +2,18 @@ const helmet = require('helmet');
 const express = require('express');
 const app = express();
 
+app.disable("x-powered-by"); /* added just in case necessary */
 
+app.get("/_api/package.json", function (req, res, next) {
+  fs.readFile(__dirname + "/package.json", function (err, data) {
+    if (err) return next(err);
+    res.type("txt").send(data.toString());
+  });
+});
+
+app.use(function (req, res, next) {
+  res.status(404).type("txt").send("Not Found");
+});
 
 
 
